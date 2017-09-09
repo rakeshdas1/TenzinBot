2cd808324665413f4db606662efc946e097bddea
Rakesh Das <rakeshdaskowshik@gmail.com>
2017-09-08 19:12:43 -0500

    express

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ require('dotenv').config();
 const snoowrap = require('snoowrap');
 const snoostorm = require('snoostorm');
 const moment = require('moment');
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 const r = new snoowrap({
     userAgent: 'reddit-bot-tenzin',
